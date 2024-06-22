174567f4552b0e0a0729c409e4361d75fa520544
Karueli <loganlem2024+1@gmail.com>
2024-06-22 21:24:09 +0200

    Add files via upload

---
+++ src/events/ready.js
@@ -0,0 +1,18 @@
+const { ActivityType, EmbedBuilder } = require('discord.js');
+const client = require('..');
+const { fetch } = require('@cloee/fetchs');
+client.on('ready', async () => {
+
+    const activityList = [
+        { name: `CorlyBot`, type: ActivityType.Watching }
+    ];
+
+    let i = 0;
+    setInterval(() => {
+        if (i >= activityList.length) i = 0;
+        client.user.setActivity(activityList[i]);
+        i++;
+    }, 10000);
+
+    console.log(`Client - Connected as ${client.user.tag}`);
+});
